951cc5a50d5fca85c406139881a9a82c69dbb49b
GAS <gaslowpoly@gmail.com>
2022-03-17 19:56:28 +0300

    init commit

---
+++ Task_0/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+
+Console.WriteLine("Set number for square");
+int number = Convert.ToInt32(Console.ReadLine());
+int sq = number * number;
+Console.WriteLine("Number is " + number + ". " + "Square is " + sq + ".");
---
+++ Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.Clear();
+Console.WriteLine("Set number A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("A = " + a + ". Now set number B");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("A = " + a + ". B = " + b + ". Let's check squares");
+
+if (b * b == a)
+{
+    Console.WriteLine("A is square of B");
+}
+else
+{
+    Console.WriteLine("A is not square of B.");
+    Console.WriteLine("Let's check B...");
+    if (a * a == b)
+    {
+        Console.WriteLine("B is square of A");
+    }
+    else
+    {
+        Console.WriteLine("B is not square of A.");
+    }
+
+}
