3b62bf2e881856bf8a4078d3e8d744e966b02b8c
GAS <GasLowPoly@gmail.com>
2022-05-01 20:52:40 +0300

    Task 10 homework done

---
+++ Task_10_Homework/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе выводит перевернутое число
+// 456 -> 654
+// 782 -> 287
+
+Console.WriteLine("Type anytheng for reverse");
+string line = Console.ReadLine();
+ReverseLine(line);
+
+void ReverseLine(string strng)
+{
+    int length = line.Length;
+    int i = length - 1;
+    while (i != -1)
+    {
+        Console.Write(line[i]);
+        i--;
+    }
+}
