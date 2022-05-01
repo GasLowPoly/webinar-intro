27ab350e46ae7e817cd280bf0281a01a0e1a1a50
GAS <GasLowPoly@gmail.com>
2022-05-01 19:19:58 +0300

    Task 14 added

---
+++ Task_14/Program.cs
@@ -0,0 +1,36 @@
+﻿// напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+void Multiple7and23(int current_number)
+{
+    if (current_number % 7 == 0)
+    {
+        Console.Write(current_number+" can be devided by 7");
+        if (current_number % 23 == 0)
+        {
+            Console.Write(" and 23");
+            
+        }
+        else
+        {
+            Console.Write(", but not 23");
+        }
+    }
+    else
+    {
+        Console.Write(current_number+" can't be devided by 7");
+        if (current_number % 23 == 0)
+        {
+            Console.Write(", but possible to devide "+current_number+" by 23");
+        }
+        else
+        {
+            Console.Write(" and 23");
+        }
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Set any number for check");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Multiple7and23(number);
