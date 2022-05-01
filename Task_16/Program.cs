612c73a74b0de33543095ba687a1ec88b8bac119
GAS <GasLowPoly@gmail.com>
2022-05-01 20:00:22 +0300

    Task 16 added

---
+++ Task_16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли второе число квадратом первого
+
+Console.WriteLine("Set 1st number for check");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Set 2nd number for check");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void CheckSqr(int a, int b)
+{
+    if (a == b * b || b == a * a)
+    {
+        Console.WriteLine("one of these numbers is square of another ");
+    }
+    else
+    {
+        Console.WriteLine("No squares here");
+    }
+}
+
+CheckSqr(number1, number2);
