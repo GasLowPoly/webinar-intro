b1562c8a201b4f2f54d415642efc5d4e838b35be
GAS <GasLowPoly@gmail.com>
2022-05-01 18:27:20 +0300

    tasks 9 and 11 added

---
+++ Task_11/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите подпрограмму, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+// пример: 456 -> 5 / 782 -> 8 / 918 -> 1
+
+
+/* через выбор символа из строки
+string number = Console.ReadLine();
+while (number.Length !=3)
+{
+    Console.WriteLine("incorrect number");
+    number = Console.ReadLine();
+}
+Console.WriteLine(number[1]);
+*/
+
+/* через 
+Console.WriteLine("Введите трёхзначное число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(a / 10 % 10);
+*/
+
+Console.WriteLine("Set 3-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondOf3(int SecondDigit) //метод выборки второй цифры в трехзначном числе
+{
+    SecondDigit = number / 10 % 10;
+    return SecondDigit;
+}
+
+int CheckLength(int length) // проверка длины числа
+{
+    string num = Convert.ToString(number);
+    length = num.Length;
+    return length;
+}
+
+if (CheckLength(number) == 3)
+{
+    Console.WriteLine("Second digit is " + SecondOf3(number));
+}
+else
+{
+    Console.WriteLine("Wrong number");
+}
--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -1,5 +1,29 @@
-﻿/* See 
-https://aka.ms/new-console-template 
-for more information
-*/
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит 10 случайных чисел из отрезка [10, 99] и показывае наибольшую цифру каждого числа
+
+
+Random rand = new Random();
+int number = rand.Next();
+
+void FindMaxDigit(int current_number) // выбираем наибольшее число и выводим результат
+{
+    int t_place = current_number / 10; //десятки числа number
+    int d_place = current_number % 10; //единицы числа number
+
+    if (t_place > d_place)
+    {
+        Console.WriteLine(number + " -> " + t_place);
+    }
+    else 
+    {
+        Console.WriteLine(number + " -> " + d_place);
+    }
+}
+
+for (int i=1;i<=10;i++) // цикл для перебора десяти цифр и вывода результатов через метод построчно
+{
+    number = rand.Next(10,100);
+    FindMaxDigit(number);
+}
+
+
+
