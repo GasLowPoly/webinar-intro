508b9608daeae0f8bc5b8f119a706d5d3bcb01de
GAS <GasLowPoly@gmail.com>
2022-05-02 22:13:52 +0300

    Task 23 done

---
+++ Task_23_Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+// Прим: 3 -> 1, 4, 9
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(n+" -> 1");
+
+for (int i = 2; i <= n; ++i)
+{
+    Console.Write($", {Math.Pow(i,3)}");
+}
