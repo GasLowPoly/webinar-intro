c362881644d0631de085e3871a40d9e6dd3c3c1d
GAS <GasLowPoly@gmail.com>
2022-05-02 21:54:39 +0300

    Task 22 done

---
+++ Task_22/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; --n)
+{
+    string msg = "______";
+    msg = n+"|"+Math.Pow(n,2);
+    Console.WriteLine(msg);
+}
