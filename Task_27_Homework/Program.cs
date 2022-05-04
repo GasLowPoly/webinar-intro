bf9d82ccccb296dc0aa413a7ddca15b1b76b7445
GAS <GasLowPoly@gmail.com>
2022-05-04 19:39:38 +0300

    Task 27 Homework done

---
+++ Task_27_Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+// 452 -> 11; 82 -> 10
+
+Console.WriteLine("Set number");
+string line = Console.ReadLine();
+try
+{
+    int number = Convert.ToInt32(line);
+    int summ = 0;
+    while (number > 0)
+    {
+        summ += number % 10;
+        number /= 10;
+    }
+    Console.WriteLine($"Summ of all digits in your number ({line}) is {summ}");
+}
+catch
+{
+    Console.WriteLine("Invalid symbols, next time set a number");
+}
