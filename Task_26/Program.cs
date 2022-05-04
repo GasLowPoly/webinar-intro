a571eb6a7e3de9dabb2c630b5c0f2dd68b15fd1c
GAS <GasLowPoly@gmail.com>
2022-05-04 16:37:25 +0300

    Task 26 done

---
+++ Task_26/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+Console.WriteLine("Set any number");
+string line =Console.ReadLine();
+try
+{
+int number = Convert.ToInt32(line);
+int numlenght = line.Length;
+Console.WriteLine(numlenght);
+}
+catch
+{
+    Console.WriteLine("This is not number, try again");
+}
