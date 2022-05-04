b585b8556cf40d0d88f7ce6e5a80330f2689a067
GAS <GasLowPoly@gmail.com>
2022-05-04 18:29:07 +0300

    Task 25 Homework done

---
+++ Task_25_Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите ЦИКЛ, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В
+
+Console.WriteLine("Set A = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Set B = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+    result *= a;
+}
+Console.WriteLine($"A^B = {result}");
