05df5841b1a641ba8db73f26bfd83c868413cee5
GAS <GasLowPoly@gmail.com>
2022-05-04 16:28:34 +0300

    Task 24 done

---
+++ Task_24/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+
+int number = Convert.ToInt32(Console.ReadLine());
+int AllSumm(int x)
+{
+    int summ = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        summ += i;
+    }
+    return summ;
+}
+Console.WriteLine(AllSumm(number));
