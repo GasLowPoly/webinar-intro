3867cf9b92c5db7e9a4c7bef1fb8639c878bc880
GAS <GasLowPoly@gmail.com>
2022-05-04 16:55:12 +0300

    Task 28 done

---
+++ Task_28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает произведение чисел от 1 до А
+
+int number = Convert.ToInt32(Console.ReadLine());
+int Factorial(int x)
+{
+    int result = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+Console.WriteLine(Factorial(number));
+
